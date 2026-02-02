bc2d5d9876bf990dab740214bcc008425643cc68
AakashNandi <aakashnandi3@gmail.com~>
2026-02-02 06:02:03 +0000

    JaiShreeKrishna

--- src/test/java/Runners/TestRunner.java
+++ src/test/java/Runners/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
     @CucumberOptions(
             features = "src/test/resources/features",
             glue = {"steps", "hooks"},  //used to specify the locations of step definition and hook files.
-            plugin = {"pretty","html:target/testng-html-report.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+            plugin = {"pretty","html:target/cucumber-html-report.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
             monochrome = true
     )
     public class TestRunner extends AbstractTestNGCucumberTests {   //base class provided by Cucumber to run tests with the TestNG framework.
